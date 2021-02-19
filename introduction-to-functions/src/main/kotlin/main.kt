c430f2b91fb3c7efc79b648091dc0e1aa3de8de4
robomajor <robokajor@gmail.com>
2021-02-19 16:44:12 +0100

    Made some changes in main.kt

--- introduction-to-functions/src/main/kotlin/main.kt
+++ introduction-to-functions/src/main/kotlin/main.kt
@@ -1,21 +1,22 @@
 fun main() {
 
-    infix fun Int.times(str: String) = str.repeat(this)
-    println(2 times "Bye ")
+    infix fun Int.times(str: String) = str.repeat(this)          //Defines an infix extension function on Int
+    println(2 times "Bye ")                                         //Calls the infix function
 
-    val pair = "Ferrari" to "Katrina"
+    val pair = "Ferrari" to "Katrina"                               //Creates a Pair by calling the infix function to from the standard library
     println(pair)
 
-    infix fun String.onto(other: String) = Pair(this, other)
+    infix fun String.onto(other: String) = Pair(this, other)        //different implementation of to creatively called onto
     val myPair = "McLaren" onto "Lucas"
     println(myPair)
 
     val sophia = Person("Sophia")
     val claudia = Person("Claudia")
-    sophia likes claudia
+    sophia likes claudia                                            //Infix notation also works on members functions (methods)
+    sophia.likedPeople.forEach { (println("Sophia likes: " + it.name)) }
 }
 
 class Person(val name: String) {
     val likedPeople = mutableListOf<Person>()
-    infix fun likes(other: Person) { likedPeople.add(other) }
+    infix fun likes(someone: Person) { likedPeople.add(someone) }      //The containing class becomes the first parameter
 }
