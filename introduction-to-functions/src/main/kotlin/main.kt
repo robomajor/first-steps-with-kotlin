f7c95f2aad89ac0f326700961ac7af1a1e80b85b
robomajor <robokajor@gmail.com>
2021-02-20 23:13:25 +0100

    Added some line about operator functions and vararg

--- introduction-to-functions/src/main/kotlin/main.kt
+++ introduction-to-functions/src/main/kotlin/main.kt
@@ -14,6 +14,30 @@ fun main() {
     val claudia = Person("Claudia")
     sophia likes claudia                                            //Infix notation also works on members functions (methods)
     sophia.likedPeople.forEach { (println("Sophia likes: " + it.name)) }
+
+
+    operator fun String.get(range: IntRange) = substring(range)     //An operator function allows easy range access on strings
+    val str = "Always forgive your enemies; nothing annoys them so much."
+    println(str[0..14])
+
+    fun printAll(vararg messages: String) {                         // The vararg modifier turns a parameter into a vararg
+        for (m in messages) println(m)
+    }
+    printAll("Hello", "Hallo", "Salut", "Hola", "你好")    //This allows calling printAll with any number of string arguments
+
+    fun printAllWithPrefix(vararg messages: String, prefix: String) {
+        for (m in messages) println(prefix + m)
+    }
+    printAllWithPrefix(
+        "yellow", "curved", "fruits", "tasty", "a deadly weapons",
+        prefix = "Bananas are "
+    )
+
+    fun log(vararg entries: String) {
+        printAll(*entries)
+    }
+
+    log("just", "some", "test", "words")
 }
 
 class Person(val name: String) {
